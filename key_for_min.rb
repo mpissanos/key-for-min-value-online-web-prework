a7458ca2fbadcf7e0ee6677f13fffd2dbf577689
Manny Pissanos <mpissano@gmail.com>
2018-09-20 16:25:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
 min_name = nil
-min = 99999999
-name_hash.each do |key, value|
-  if value <= min
-    min = value
-    min_name = key
+min_price = 99999999
+name_hash.each do |obj, value|
+  if value <= min_price
+    min_price = value
+    min_name = obj
 end
   end
   min_name
